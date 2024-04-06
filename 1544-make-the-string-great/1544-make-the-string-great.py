94065135ea831cc481419fed266fd93ed1e55771
Vaishnavi Nevkar <90646128+Vyshnavii-19@users.noreply.github.com>
2024-04-06 23:03:44 +0530

    Time: 46 ms (9.95%), Space: 16.7 MB (11.18%) - LeetHub

---
+++ 1544-make-the-string-great/1544-make-the-string-great.py
@@ -0,0 +1,12 @@
+# class Solution:
+class Solution(object):
+    def makeGood(self, s: str) -> str:
+        stack = []
+
+        for i in range(len(s)):
+            if not stack == []:
+                if  abs(ord(stack[-1])-ord(s[i])) == 32:
+                    stack.pop()
+                    continue
+            stack.append(s[i])
+        return ''.join(stack)
